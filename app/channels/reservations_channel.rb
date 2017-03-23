b71972f6310d4f664128ac4db4bf5c2d830c7706
Luka Domitrovič <luka@domitrovic.si>
2017-03-23 15:21:45 +0100

    edit reservation channel

--- app/channels/reservations_channel.rb
+++ app/channels/reservations_channel.rb
@@ -7,13 +7,14 @@ class ReservationsChannel < ApplicationCable::Channel
   def reserve(data)
     date = DateTime.parse(data['date']).in_time_zone("CET") rescue nil
     reservations = Reservation.where('start_on BETWEEN ? AND ?', date.beginning_of_day, date.end_of_day)
-    slots = (0..24).to_a
+    slots = (0..23).to_a
     unavailable_slots = []
     reservations.each do |r|
        unavailable_slots << r.start_on.hour
        unavailable_slots << r.start_on.hour + 1
     end
-    available_slots = (slots - unavailable_slots) << 0
+    available_slots = slots - unavailable_slots
+    available_slots.map! {|x| date.change({ hour: x, min: 0, sec: 0 })}
     ActionCable.server.broadcast 'reservations_channel', slots: render_slots(available_slots)
   end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :customer
 
-  def reservation_slot
+  def reservation_slot=(slot)
+    hours = slot.split('#')
+    self.start_on = hours.first
+    self.end_on = hours.last
+  end
 
+  def reservation_slot
   end
 end
