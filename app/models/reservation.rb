9214adb50c0212b0ff2c91b70cd77b4e4b3c291f
Luka Domitrovič <luka@domitrovic.si>
2017-03-23 15:26:43 +0100

    add reservation validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,14 @@
 class Reservation < ApplicationRecord
   belongs_to :customer
 
+  validate :start_on_date_cannot_be_in_the_past
+
+  def start_on_date_cannot_be_in_the_past
+    if start_on.present? && start_on < Date.today
+      errors.add(:start_on, "can't be in the past")
+    end
+  end
+
   def reservation_slot=(slot)
     hours = slot.split('#')
     self.start_on = hours.first
