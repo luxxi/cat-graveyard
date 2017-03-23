0222622bea9a5e90e719190f2e2b0769b2696f6c
Luka Domitrovič <luka@domitrovic.si>
2017-03-23 13:38:02 +0100

    edit before authenticate

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,12 @@
 class ReservationsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
     @reservations = Reservation.all
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
